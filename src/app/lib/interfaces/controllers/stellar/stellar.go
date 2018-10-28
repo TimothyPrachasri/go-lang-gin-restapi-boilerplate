426330d96655b6876707186e9b69cd1a4e06bbce
Tim Nantawat <nantawat@dek-d.com>
2018-10-28 19:01:28 +0700

    fix(api/stellar): change from seeding source and destination to receiving a real form input from user

--- src/app/lib/interfaces/controllers/stellar/stellar.go
+++ src/app/lib/interfaces/controllers/stellar/stellar.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	service "app/lib/domain/services/stellar"
@@ -9,13 +10,17 @@ import (
 )
 
 func Transfer(c *gin.Context) {
-	sourceSeed := "SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME"
-	sourceAddr := "GAMMEG4YSIGJO3I45T37A57EN5CFQQH5OVHO4XDUGF7E32VJ76YYX56E"
-	destinationAddr := "GCICVEBF5JYDBCTR3TXFGN56WGYBAKKWVHUQYPM72F6ZEQ7BDQZT4NFZ"
-	addresses := [2]string{sourceAddr, destinationAddr}
-	service.Stellar{}.LogBalances(addresses)
-	status, err := service.Stellar{}.SendLumens("100", sourceSeed, destinationAddr)
-	service.Stellar{}.LogBalances(addresses)
+	from := c.PostForm("from")
+	to := c.PostForm("to")
+	amount := c.PostForm("amount")
+	fmt.Println(from, to, amount, "test")
+	isValid := from != "" || to != "" || amount != ""
+	if !isValid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "Insufficient Form Request",
+		})
+	}
+	status, err := service.Stellar{}.SendLumens(amount, from, to)
 	if status {
 		c.JSON(http.StatusOK, gin.H{
 			"status": status,
@@ -26,56 +31,3 @@ func Transfer(c *gin.Context) {
 		"err": err,
 	})
 }
-
-// func logBalances(addresses [2]string) {
-// 	for _, address := range addresses {
-// 		account, err := horizon.DefaultTestNetClient.LoadAccount(address)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		log.Println("Balances for address:", address)
-// 		for _, balance := range account.Balances {
-// 			log.Println(balance)
-// 		}
-// 	}
-// }
-
-// func sendLumens(amount string, sourcePrivateKey string, destinationPublicKey string) (bool, error) {
-// 	if _, err := horizon.DefaultTestNetClient.LoadAccount(destinationPublicKey); err != nil {
-// 		return false, err
-// 	}
-
-// 	tx, err := build.Transaction(
-// 		build.TestNetwork,
-// 		build.SourceAccount{sourcePrivateKey},
-// 		build.AutoSequence{horizon.DefaultTestNetClient},
-// 		build.Payment(
-// 			build.Destination{destinationPublicKey},
-// 			build.NativeAmount{amount},
-// 		),
-// 	)
-
-// 	fmt.Println(tx, "tx")
-
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	txe, err := tx.Sign(sourcePrivateKey)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	txeB64, err := txe.Base64()
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	resp, err := horizon.DefaultTestNetClient.SubmitTransaction(txeB64)
-// 	if err != nil {
-// 		return false, err
-// 	}
-
-// 	log.Println("Successfully sent", amount, "lumens to", destinationPublicKey, ". Hash:", resp.Hash)
-// 	return true, nil
-// }
